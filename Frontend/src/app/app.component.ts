4a11937567855076f95b0a387f9462748b612a9a
harsha.kondreddi <harsha.kondreddi@endowus.com>
2025-06-08 21:29:44 +0530

    changes for running the BE & FE locally

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: "app-root",
@@ -11,9 +12,12 @@ export class AppComponent implements OnInit {
   title = "spMgAngDocker";
   constructor(private http: HttpClient) {}
   data: any = [];
+  newItemText: string = '';
+
   ngOnInit() {
     this.getData();
   }
+
   getData() {
     let url = "http://localhost:8080/getItems";
     this.http.get(url).subscribe((res) => {
@@ -21,9 +25,12 @@ export class AppComponent implements OnInit {
       this.data = res;
     });
   }
+
   addItem() {
+    if (!this.newItemText.trim()) return;
+    
     let url = "http://localhost:8080/addItem";
-    let data = { itemName: "new item" };
+    let data = { itemName: this.newItemText };
     this.http
       .post(url, data, {
         headers: {
@@ -33,6 +40,7 @@ export class AppComponent implements OnInit {
       .subscribe((res) => {
         console.log(res);
         this.getData();
+        this.newItemText = ''; // Clear the input after adding
       });
   }
 }
